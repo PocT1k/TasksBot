3293f4d4e58941ff712427c7153c6c471ae28681
Kirill Novikov <novikovk2003@yandex.ru>
2023-10-30 00:33:19 +0300

    del #

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, types
 
 TOKEN_API = "6781243626:AAF05GPR4Y1rS2Brm42kjN97tNx0UhRrC0I"
 DATA_BASE = "data.db"
-#1144499287 #854903492
+#Дима:1144499287 #Кирилл:854903492
 
 # Инициализация бота и диспетчера
 bot = Bot(TOKEN_API)
